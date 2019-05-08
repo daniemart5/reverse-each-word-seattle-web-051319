6e06e372930191e2d8fef0b6aada4443d1bafa62
Danielle Martinoli <daniechris5@gmail.com>
2019-05-08 23:17:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,3 +5,11 @@ def reverse_each_word(sentence1)
     end
     new_array.join(" ")
 end
+
+def reverse_each_word(sentence1)
+    new_array = sentence1.split
+    new_array.collect do |word|
+      word.reverse!
+    end
+    new_array.join(" ")
+end
